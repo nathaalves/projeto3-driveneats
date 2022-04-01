5f55aa5fde092d85a391f92b685c3be99d9e7a50
nathaalves <nathaalvesaraujo@gmail.com>
2022-04-01 10:16:37 -0300

    inclusão do ícone 'checked' na opção selecionada

--- scripts.js
+++ scripts.js
@@ -1,16 +1,13 @@
-//function selectedCategori() {
-//    selected = document.querySelector(".dishes").querySelector(".option")
-//    console.log(selected)
-//}
-
-
-
-
-
 function selectOption(option) {
+
     selected = document.querySelector(`.${option.parentElement.className} .selectedOption`);
+    checked = document.querySelector(`.${option.parentElement.className} .checkedOption`)
     if (selected !== null) {
       selected.classList.remove("selectedOption");
+      checked.classList.remove("checkedOption");
     }
+    
     option.classList.add("selectedOption");
+    option.querySelector("ion-icon").classList.add("checkedOption")
 }
+
